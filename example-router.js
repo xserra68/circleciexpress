2a59248f5135eb205296eae12fd2a75ccb5a0f51
Xavier Serra <45533360+xserra68@users.noreply.github.com>
2021-03-18 21:50:22 +0100

    Nuevo commit 3

--- example-router.js
+++ example-router.js
@@ -1,4 +1,4 @@
 function exampleRouteHandler(req, res) {
-  res.send("Goodbye Earthling!");
+  res.send("Godbye Earthling!");
 }
 module.exports = exampleRouteHandler;
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
 describe("Example Test 1", () => {
-  it("should return 'Goodbye Earthling!' for GET /example", () => {
+  it("should return 'Godbye Earthling!' for GET /example", () => {
     const mockRequest = httpMocks.createRequest({
       method: "GET",
       url: "/example"
