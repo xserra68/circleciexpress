443b1410055f4741ad51d74f8b16fa41d53344e7
Xavier Serra <45533360+xserra68@users.noreply.github.com>
2021-03-21 19:33:33 +0100

    example2

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
-describe("Example Test 1", () => {
+describe("Example Test 2", () => {
   it("should return 'Goodbye Earthling!' for GET /example", () => {
     const mockRequest = httpMocks.createRequest({
       method: "GET",
